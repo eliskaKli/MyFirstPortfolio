5b581df43e2e1eeae92a41575511d8330b8095d7
eliskaKli <eliskaklimtova@seznam.cz>
2023-05-04 22:00:45 +0200

    Styles in css & changes in html.

---
+++ script.js
@@ -0,0 +1,26 @@
+// console.log('čusík'); // tohle  jako jediné funguje :-(
+
+// const buttons = document.querySelectorAll('button');
+// console.log(buttons);    
+
+// const name = document.getElementById('nameEK');
+// console.log(name);
+    
+// document.getElementById('nameEK').innerHTML='Pampík';
+// document.querySelector('h1').innerHTML='JavaScript propojen :-)';
+
+// document.getElementsByClassName('button').style.background='yellow';
+
+
+
+// Po načtení stránky se udělá animace podbarvení .container-header
+
+// function changeBackgroundColor() {
+//     document.getElementsByClassName('container-header').style.background = 'linear-gradient(90deg, #4F0E1A 0%, #443A53 21.92%, #3184B4 110.74%)';
+// }
+
+// let header = document.getElementByClassName('container-header');
+// header.onclick = changeBackgroundColor;
+// header.timeStamp
+
+// About me - zobrazuje se správně #desktop/#tabletMobile
